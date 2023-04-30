3d5d544d5a4cc9f1c197b80bb310c99800339283
Lucas de Oliveira <lucas.g.oliveira1998@outlook.com>
2023-04-30 13:25:35 -0300

    Adicionando modificações a classe 'HelloWorld.java'.

--- Git-Aula-2023-1/src/HelloWorld.java
+++ Git-Aula-2023-1/src/HelloWorld.java
@@ -1,9 +1,12 @@
 
-public class HelloWorld {
-
-	public static void main(String[] args) {
+public class HelloWorld
+{
+	public static void main(String[] args)
+	{
 		System.out.println("Hello World!!!");
 		System.out.println("Copia Bash + Eclipse");
+		
+		System.out.println("Lucas de Oliveira -> Fazendo modificações aqui.");
+		System.out.println("Lucas de Oliveira -> Hello World!!!");
 	}
-
 }

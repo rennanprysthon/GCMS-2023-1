e208eb1859b55fe89233de9345a85947aef95757
Ramide Dantas <ramidedantas@recife.ifpe.edu.br>
2023-05-05 11:29:05 -0300

    Clean up

--- Git-Aula-2023-1/src/HelloWorld.java
+++ Git-Aula-2023-1/src/HelloWorld.java
@@ -6,16 +6,8 @@ public class HelloWorld
 		System.out.println("Hello World!!!");
 		System.out.println("Copia Bash + Eclipse");
 		
-		System.out.println("Lucas de Oliveira -> Fazendo modificações aqui.");
 		System.out.println("Lucas de Oliveira -> Hello World!!!");
-		
-		
-		System.out.println();
-		
-		System.out.println("=================================================");
-		System.out.println("Lilian Beatriz Fazendo alterações neste arquivo");
-		
-		System.out.println("GitHub: @beatrizgomess - Hello World!!!!");
+		System.out.println("Lilian Beatriz Fazendo alteracoes neste arquivo");
 		
 	}
 }

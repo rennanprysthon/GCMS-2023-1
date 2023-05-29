37020fff94dd820f46142018788cbee1574f7a46
Milenamelotavares <milenamelo0908@gmail.com>
2023-05-29 20:02:55 -0300

    Pratica 3 - alterando o arquivo Hello World - Lucas André (@MoonEye696)

--- Git-Aula-2023-1/src/HelloWorld.java
+++ Git-Aula-2023-1/src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
 		System.out.println("Hello World!!!");
 		System.out.println("Copia Bash + Eclipse");
 		System.out.println("Branch Bash + Eclipse");
-		System.out.println("Mudan�a para PR reverso!");
+		System.out.println("Mudan�a para PR reverso!");
 		
 		System.out.println("Lucas de Oliveira -> Hello World!!!");
 		System.out.println("[branch: Lucas-Oliveira] -> Realizando mais alterações.");
@@ -24,6 +24,8 @@ public class HelloWorld
 		System.out.println("Rafael Moraes -> Hello World!!!");
 		System.out.println("Rennan Prysthon -> Hello World! via Pull Request");
 		System.out.println("JeozadaqueMacedo -> Hello World!");
+		
+		System.out.println("Lucas André -> Hello World!/ Prática 03");
 		
 	}
 }
